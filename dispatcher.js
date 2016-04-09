ae2e3c117b96c01acb8fdd82edbc38e622a88ff1
mbilker <me@mbilker.us>
2016-04-09 06:38:21 +0000

    dispatcher: commented tap to inspect flowing events

--- dispatcher.js
+++ dispatcher.js
@@ -1,6 +1,13 @@
+const debug = require('debug')('hubot-dispatcher');
 const EventEmitter = require('events').EventEmitter;
 
 const dispatcher = new EventEmitter();
+
+// const _emit = dispatcher.emit;
+// dispatcher.emit = function emit(ev) {
+//   debug.apply(null, arguments);
+//   return _emit.apply(dispatcher, arguments);
+// }
 
 process.on('SIGINT', function() {
   dispatcher.emit('ctrlc');
