65651264019901593cf7ee8dfc3b99f997e045a4
mbilker <me@mbilker.us>
2016-04-12 01:05:24 +0000

    settings: change voice channel room name

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
   SERVER_NAME: 'Obsidian Bears',
   TEXT_CHANNEL: 'hubot-songlist',
-  VOICE_CHANNEL: 'Hubot\'s DJ Booth',
+  VOICE_CHANNEL: 'Cardinal Booth',
 
   STATUS_DELAY_TIME: 3000,
   STREAM_VOLUME: 0.2
