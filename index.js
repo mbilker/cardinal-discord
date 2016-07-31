c7ec94b10556cebd75997ea508bc1673eea41bae
mbilker <me@mbilker.us>
2016-07-31 07:50:39 +0000

    repl: add repl to introspect without using eval command

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ debug.enable('hubot*,cardinal*');
 
 //require('./icy.js');
 //require('./mpd.js');
-require('./queue.js');
-require('./nicehash.js');
-require('./status.js');
+const queue = require('./queue.js');
+const nicehash = require('./nicehash.js');
+const status = require('./status.js');
 //require('./web.js');
 
 const bot = require('./bot.js');
 bot.start();
+
+const repl = require('repl').start('> ');
+repl.context.Module = require('./Core/Module');
+repl.context.Command = require('./Core/Command');
+repl.context.CommandManager = require('./Core/CommandManager');
+
+repl.context.queue = queue;
+repl.context.nicehash = nicehash;
+repl.context.status = status;
