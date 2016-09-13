9e857a0b93fe1c18b8000c691881bd95b47dabc0
mbilker <me@mbilker.us>
2016-09-13 22:58:38 +0000

    Dispatcher: add line to detect inclusion, disable ctrl-c handling

--- dispatcher.js
+++ dispatcher.js
@@ -3,14 +3,16 @@ const EventEmitter = require('events').EventEmitter;
 
 const dispatcher = new EventEmitter();
 
+debug('loaded');
+
 // const _emit = dispatcher.emit;
 // dispatcher.emit = function emit(ev) {
 //   debug.apply(null, arguments);
 //   return _emit.apply(dispatcher, arguments);
 // };
 
-process.on('SIGINT', function() {
-  dispatcher.emit('ctrlc');
-});
+//process.on('SIGINT', function() {
+//  dispatcher.emit('ctrlc');
+//});
 
 module.exports = dispatcher;
