e4b301138d3f52b66dad558e2314855b9222bb77
mbilker <me@mbilker.us>
2016-09-15 05:35:50 +0000

    API/Module: remove deprecated registerCommand methods

--- Core/API/Module.js
+++ Core/API/Module.js
@@ -1,27 +1,11 @@
 "use strict";
 
-const Command = require('./Command');
-
 class Module {
   constructor(container) {
     this.container = container;
 
     this.logger = container.get('logger');
     this.commandManager = container.get('commandManager');
-  }
-
-  registerCommand(cmd) {
-    if (!(cmd instanceof Command)) {
-      throw new Error('registerCommand(cmd): cmd not instance of Command');
-    }
-
-    return this.commandManager.add(this, cmd);
-  }
-
-  registerCommands(cmds) {
-    for (const cmd of cmds) {
-      this.registerCommand(cmd);
-    }
   }
 
   hears(cmd, func) {
