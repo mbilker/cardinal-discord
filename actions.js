521342537776a5736f6ee7076e2cd1efbd8bafc5
mbilker <me@mbilker.us>
2016-10-17 06:46:25 +0000

    Actions: remove translated APIs

--- actions.js
+++ actions.js
@@ -1,17 +1,9 @@
 const keyMirror = require('keymirror');
 
 const Actions = keyMirror({
-  DISCORD_READY: null,
-  DISCORD_FOUND_CORRECT_SERVER: null,
-  DISCORD_FOUND_VOICE_CHANNEL: null,
-  DISCORD_JOINED_VOICE_CHANNEL: null,
-
   START_MUSIC_PLAYBACK: null,
   ICY_CONNECTED: null,
   ICY_METADATA: null,
-
-  STATUS_SYS_INFO: null,
-  STATUS_USER_INFO: null,
 });
 
 module.exports = Actions;
