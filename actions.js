15ac5c9ef3782c7bcb42a25d8c8caea0ffba128c
mbilker <me@mbilker.us>
2016-11-09 07:38:08 +0000

    Actions: remove since not included anywhere

--- actions.js
+++
@@ -1,9 +0,0 @@
-const keyMirror = require('keymirror');
-
-const Actions = keyMirror({
-  START_MUSIC_PLAYBACK: null,
-  ICY_CONNECTED: null,
-  ICY_METADATA: null,
-});
-
-module.exports = Actions;
