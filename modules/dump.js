dddf1ebaad64f3f767d4ebab6e47722334136c9e
mbilker <me@mbilker.us>
2016-11-21 09:52:30 +0000

    DumpCommand: begin processing as messages are received

--- modules/dump.js
+++ modules/dump.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const crypto = require('crypto');
+
 const followRedirects = require('follow-redirects');
 const { http, https } = followRedirects;
 const url = require('url');
@@ -7,6 +9,7 @@ const url = require('url');
 const Module = require('../Core/API/Module');
 
 const REGEX = /<#([0-9]+)>/;
+const EMPTY_IMAGE = '';
 
 class BackupCommand extends Module {
   constructor(container) {
@@ -16,7 +19,9 @@ class BackupCommand extends Module {
 
     this._inProgress = new Map();
     this._avatarCache = {};
-    this._messages = {};
+
+    this._agent = new http.Agent({ keepAlive: false, maxSockets: 500 });
+    this._secureAgent = new https.Agent({ keepAlive: false, maxSockets : 500 });
 
     this.hears(/backup/i, this.onBackupCommand.bind(this));
   }
@@ -39,12 +44,18 @@ class BackupCommand extends Module {
   }
 
   fetchImage(imageURL) {
+    if (!imageURL) {
+      return Promise.reject('imageURL cannot be null');
+    }
+
     const options = url.parse(imageURL);
     const httpObject = options.protocol === 'http:' ? http : https;
 
+    options.agent = options.protocol === 'http:' ? this._agent : this._secureAgent;
+
     return new Promise((resolve, reject) => {
-      httpObject.get(imageURL, (res) => {
-        this.logger.debug(`fetchImage res ${imageURL}`);
+      const req = httpObject.request(options, (res) => {
+        //this.logger.debug(`fetchImage res ${imageURL}`);
 
         const buffers = [];
         res.on('data', (chunk) => {
@@ -58,24 +69,66 @@ class BackupCommand extends Module {
         res.on('error', (err) => {
           reject(err);
         });
-      }).on('error', (err) => {
+      });
+
+      req.on('error', (err) => {
         reject(err);
       });
+
+      req.end();
     });
   }
 
-  fetchAvatar(user) {
-    if (this._avatarCache[user.id]) {
-      return this._avatarCache[user.id];
+  fetchAvatar({ avatarURL, id }) {
+    if (this._avatarCache[id]) {
+      return this._avatarCache[id];
+    } else if (!avatarURL) {
+      return Promise.resolve(EMPTY_IMAGE);
     }
 
-    const promise = this.fetchImage(user.avatarURL).then((base64) => {
-      this.logger.debug(`avatar retrieved ${user.id}`);
+    const redisKey = this.getRedisKey('avatar', id);
+
+    const promise = this.redisClient.getAsync(redisKey).then(([ avatar ]) => {
+      if (avatar) {
+        this.logger.debug(`avatar redis exists ${id}`);
+        return avatar;
+      }
+      return this.fetchImage(avatarURL);
+    }).then((base64) => {
+      this.logger.debug(`avatar retrieved ${id}`);
+      if (base64) {
+        return this.redisClient.setAsync(redisKey, base64).then(() => base64);
+      }
       return base64;
+    }, (err) => {
+      this.logger.error(`avatar fetch failed ${id} ${avatarURL}`);
+      return EMPTY_IMAGE;
     });
-    this._avatarCache[user.id] = promise;
+    this._avatarCache[id] = promise;
 
     return promise;
+  }
+
+  fetchOtherImage(imageURL) {
+    const redisKey = this.getRedisKey('image', crypto.createHash('sha256').update(imageURL).digest('hex'));
+
+    return this.redisClient.getAsync(redisKey).then(([ image ]) => {
+      if (image) {
+        this.logger.warn(`other image redis exists ${imageURL}`);
+        return image;
+      }
+      return this.fetchImage(imageURL);
+    }).then((base64) => {
+      this.logger.debug(`other image retrieved ${imageURL}`);
+      if (base64) {
+        return this.redisClient.setAsync(redisKey, base64).then(() => base64);
+      }
+      return base64;
+    }, (err) => {
+      this.logger.warn(`other image failed ${imageURL}`);
+      this.logger.error(`other image error ${err}`);
+      return EMPTY_IMAGE;
+    });
   }
 
   /**
@@ -85,55 +138,82 @@ class BackupCommand extends Module {
    *    messages: {Array} array of messages in ascending time order
    *  }
    */
-  onCompletion(textChannel) {
-    const messages = this._messages[textChannel.id];
-    const info = textChannel.toJSON();
-
-    const avatars = {};
+  onPartCompletion(ctx, messages) {
     const finalized = messages.map((message) => {
       const plain = message.toJSON();
       const resolvedContent = message.resolveContent();
       plain.content = resolvedContent;
 
-      const promises = [this.fetchAvatar(message.author)];
-
-      for (const embed of message.embeds) {
-        promises.push(this.fetchImage(embed.thumbnail.url));
+      if (!ctx.avatars[message.author.id]) {
+        ctx.avatars.push(this.fetchAvatar(message.author).then((res) => [message.author.id, res]));
       }
 
-      return Promise.all(promises).then((values) => {
-        avatars[message.author.id] = values.shift();
+      const promises = message.embeds.map((embed) => {
+        if (embed && embed.thumbnail) {
+          return this.fetchOtherImage(embed.thumbnail.url);
+        } else if (embed) {
+          if (embed && (embed.type === 'video' || embed.type === 'article' || embed.type === 'tweet' || embed.type === 'image')) {
+            //this.logger.error(`no thumbnail for embed ${JSON.stringify(embed)}`);
+          } else if (embed && embed.type === 'link') {
+            //this.logger.debug('link embed found');
+          } else {
+            this.logger.error(`embed is something else ${JSON.stringify(embed)}`);
+          }
+          //return Promise.resolve(EMPTY_IMAGE);
+        }
+      });
 
+      return Promise.all(promises).then((values) => {
         for (const [i, base64] of values.entries()) {
-          plain.embeds[i].proxy_url = base64;
+          if (base64) {
+            plain.embeds[i].proxy_url = base64;
+          }
         }
 
         return plain;
       });
     });
 
-    return Promise.all(finalized).then((messages) => {
+    return finalized;
+  }
+
+  onCompletion(ctx, textChannel) {
+    const { info } = ctx;
+
+    const allPromises = [
+      Promise.all(ctx.avatars),
+      Promise.all(ctx.messages)
+    ];
+
+    return Promise.all(allPromises).then(([ zipAvatars, messages ]) => {
+      const avatars = {};
+      for (const zip of zipAvatars) {
+        avatars[zip[0]] = zip[1];
+      }
+
       const obj = { avatars, messages, info };
       return this.saveToRedis(textChannel, obj);
     });
   }
 
-  storeMessages(id, messages) {
-    this._messages[id] = this._messages[id].concat(messages.reverse());
+  storeMessages(ctx, id, messages) {
+    const reversed = messages.reverse();
+    const part = this.onPartCompletion(ctx, reversed);
+    ctx.messages = ctx.messages.concat(part);
   }
 
-  onFetch(e, textChannel) {
+  onFetch(ctx, e, textChannel) {
     if (!e.messages.length) {
       this.logger.debug('reached the end of the history');
 
       this._inProgress.delete(textChannel.id);
-      return this.onCompletion(textChannel);
+      return this.onCompletion(ctx, textChannel);
     }
 
-    this.storeMessages(textChannel.id, e.messages);
+    this.storeMessages(ctx, textChannel.id, e.messages);
 
     const before = e.messages[0];
-    return textChannel.fetchMessages(100, before).then(e => this.onFetch(e, textChannel));
+    return textChannel.fetchMessages(100, before).then(e => this.onFetch(ctx, e, textChannel));
   }
 
   onBackupCommand(m, args) {
@@ -157,13 +237,18 @@ class BackupCommand extends Module {
       return m.reply('Already backing up channel');
     }
     this._inProgress.set(textChannel.id, true);
-    this._messages[textChannel.id] = [];
+
+    const ctx = {
+      avatars: [],
+      messages: [],
+      info: textChannel.toJSON()
+    };
 
     this.logger.info(`starting backup of ${textChannel.name} (${textChannel.id})`);
     m.channel.sendMessage(`Starting backup of ${textChannel.mention}`);
-    return textChannel.fetchMessages(null, null, null).then(e => this.onFetch(e, textChannel)).then(() => {
+    return textChannel.fetchMessages(null, null, null).then(e => this.onFetch(ctx, e, textChannel)).then(() => {
       this.logger.info('all done');
-      m.channel.sendMessage(`Backup finished (${this._messages[textChannel.id].length} message(s))`);
+      return m.channel.sendMessage(`Backup finished (${ctx.messages.length} message(s))`);
     }).catch((err) => {
       this.logger.error('error with channel backup', err);
       return m.reply('Failed to complete channel backup, consult console for more information');
