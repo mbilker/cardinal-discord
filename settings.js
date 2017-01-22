646c36aac7e7d2f7e2f55037859d6fe0e0902f44
mbilker <me@mbilker.us>
2017-01-22 02:54:38 +0000

    settings: remove obsolete option STREAM_VOLUME

--- settings.js
+++ settings.js
@@ -1,10 +1,9 @@
 module.exports = Object.freeze({
   STATUS_DELAY_TIME: 3000,
-  STREAM_VOLUME: 0.2,
 
   NICEHASH_ADDRESS: '1KiMjCRxfUcwydcUo77gqTDh4sQzGVJ3P5',
 
-  MPD_HOST: 'localhost',
+  MPD_HOST: 'turret.lab.mbilker.us',
   MPD_PORT: 6600,
   MPD_BASE_DIRECTORY: '/storage/media/mpd',
 });
