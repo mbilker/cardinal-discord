148c7fd8294450d0fbd209489d1403480e741836
mbilker <me@mbilker.us>
2017-04-04 06:46:46 -0400

    settings: remove obsolete file

--- settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = Object.freeze({
-  STATUS_DELAY_TIME: 3000,
-
-  NICEHASH_ADDRESS: '1KiMjCRxfUcwydcUo77gqTDh4sQzGVJ3P5',
-
-  MPD_HOST: 'turret.lab.mbilker.us',
-  MPD_PORT: 6600,
-  MPD_BASE_DIRECTORY: '/storage/media/mpd',
-});
