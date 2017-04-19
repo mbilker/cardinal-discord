2db6a5d0b9e0939b1f4154111e4e103d2acb3b2a
mbilker <me@mbilker.us>
2017-04-18 22:52:17 -0400

    Nicehash: transition to Promise API

--- modules/nicehash.js
+++ modules/nicehash.js
@@ -14,54 +14,68 @@ class Nicehash extends Module {
     this.hears(/nice/i, this.display.bind(this));
   }
 
+  fetchApi() {
+    return new Promise((resolve, reject) => {
+      const reqUrl = `https://www.nicehash.com/api?method=stats.provider&location=1&addr=${this.nicehashAddress}`;
+      const parsed = url.parse(reqUrl);
+      const req = https.get(parsed);
+
+      const chunks = [];
+
+      req.on('error', (err) => {
+        reject(err);
+      });
+
+      req.on('response', (res) => {
+        this.logger.debug(`have response: ${res.statusCode}`);
+
+        if (res.statusCode !== 200) {
+          reject(new Error(`Status code ${res.statusCode}`));
+          return;
+        }
+
+        res.on('data', (chunk) => chunks.push(chunk));
+        res.on('end', () => {
+          const buf = Buffer.concat(chunks);
+          resolve(buf);
+        });
+      });
+    });
+  }
+
   display(m) {
     this.logger.debug('NICEHASH_DISPLAY');
 
-    const reqUrl = `https://www.nicehash.com/api?method=stats.provider&location=1&addr=${this.nicehashAddress}`;
-    const parsed = url.parse(reqUrl);
-    const req = https.get(parsed);
+    return this.fetchApi().then((buf) => {
+      const text = buf.toString();
+      let data = null;
 
-    req.once('response', (res) => {
-      this.logger.debug(`have response: ${res.statusCode}`);
-
-      let chunks = [];
-
-      if (res.statusCode !== 200) {
-        this.logger.debug('bailing out, non-200 status code');
-        m.reply('There was an error processing the response');
+      try {
+        data = JSON.parse(text);
+      } catch (e) {
+        this.logger.debug('error decoding json', e);
+        m.reply('There was an error decoding the JSON payload');
         return;
       }
 
-      res.on('data', (chunk) => chunks.push(chunk));
-      res.once('end', () => {
-        const text = Buffer.concat(chunks).toString();
-        let data = null;
+      if (!data.result.stats.length) {
+        m.reply(`No data found for ${this.nicehashAddress}`);
+        return;
+      }
 
-        try {
-          data = JSON.parse(text);
-        } catch (e) {
-          this.logger.debug('error decoding json', e);
-          m.reply('There was an error decoding the JSON payload');
-          return;
-        }
+      const stats = data.result.stats;
+      const speed = stats.reduce(((total, stats) => total + parseFloat(stats.accepted_speed) * 1000), 0);
+      const balance = stats.reduce(((total, stats) => total + parseFloat(stats.balance)), 0);
 
-        if (!data.result.stats.length) {
-          m.reply(`No data found for ${this.nicehashAddress}`);
-          return;
-        }
-
-        const stats = data.result.stats;
-        const speed = stats.reduce(((total, stats) => total + parseFloat(stats.accepted_speed) * 1000), 0);
-        const balance = stats.reduce(((total, stats) => total + parseFloat(stats.balance)), 0);
-
-        m.channel.sendMessage(`:pick: ${this.nicehashAddress}
+      m.channel.sendMessage(`:pick: ${this.nicehashAddress}
 :zap: ${speed} MH/s
 :moneybag: ${balance} BTC`);
-        for (let i = 0; i <= 2; i++) {
-          const payment = data.result.payments[i];
-          m.channel.sendMessage(`${i} _${payment.time}_\t :id: ${payment.TXID.slice(0, 10)}\t :money_with_wings: ${payment.amount}\t :arrow_down: ${payment.fee}`);
-        }
-      });
+      for (let i = 0; i <= 2; i++) {
+        const payment = data.result.payments[i];
+        if (!payment) continue;
+
+        m.channel.sendMessage(`${i} _${payment.time}_\t :id: ${payment.TXID.slice(0, 10)}\t :money_with_wings: ${payment.amount}\t :arrow_down: ${payment.fee}`);
+      }
     });
   }
 }
