f579a970ef5dfcf7ced5a4e1b89ecc72ff3c0bed
mbilker <me@mbilker.us>
2017-09-03 04:04:13 -0400

    MusicPlayer: escape query to YTDL API

--- queue/utils.js
+++ queue/utils.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const http = require('http');
+const querystring = require('querystring');
 
 const google = require('googleapis');
-//const ytdl = require('ytdl-core');
 
 const Types = require('./types');
 
@@ -13,8 +13,6 @@ const apiKey = require('../google_api.json').apiKey;
 
 const YOUTUBE_DL_SERVICE_HOSTNAME = process.env.YOUTUBE_DL_SERVICE_HOSTNAME || 'localhost';
 const YOUTUBE_DL_SERVICE_PORT = process.env.YOUTUBE_DL_SERVICE_PORT || '5000';
-
-
 
 class Utils {
   constructor(container) {
@@ -31,11 +29,12 @@ class Utils {
 
   fetchYoutubeInfo(url) {
     return new Promise((resolve, reject) => {
+      const urlPath = querystring.escape(`/download?v=${url}`);
       const req = http.request({
         method: 'GET',
         hostname: YOUTUBE_DL_SERVICE_HOSTNAME,
         port: YOUTUBE_DL_SERVICE_PORT,
-        path: '/download?v=' + url,
+        path: urlPath,
       }, (res) => {
         const buffers = [];
 
@@ -93,56 +92,3 @@ class Utils {
 }
 
 module.exports = Utils;
-
-/*
-function sortFormats(a, b) {
-  // anything towards the beginning of the array is -1, 1 to move it to the end
-  if (a.audioEncoding === 'opus' && b.audioEncoding !== 'opus') {
-    return -1;
-  } else if (a.audioEncoding !== 'opus' && b.audioEncoding === 'opus') {
-    return 1;
-  }
-
-  if (a.audioEncoding === 'vorbis' && b.audioEncoding !== 'vorbis') {
-    return -1;
-  } else if (a.audioEncoding !== 'vorbis' && b.audioEncoding === 'vorbis') {
-    return 1;
-  }
-
-  if (a.audioBitrate < b.audioBitrate) {
-    return 1;
-  } else if (a.audioBitrate > b.audioBitrate) {
-    return -1;
-  }
-
-  return 0;
-};
-
-function fetchYoutubeInfo(url) {
-  return new Promise((resolve, reject) => {
-    ytdl.getInfo(url, { filter: 'audioonly' }, (err, info) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      const formats = info.formats
-        .filter(x => x.audioEncoding)
-        .sort(sortFormats)
-        .map(x => ({
-          container: x.container,
-          url: x.url,
-          audioEncoding: x.audioEncoding,
-          audioBitrate: x.audioBitrate,
-        }));
-
-      resolve([info, formats]);
-    });
-  });
-};
-
-
-module.exports = {
-//  sortFormats,
-};
-*/
